8aaa3dc7387b448b2bdacd27f5f8573b597da185
vladimir.uram <vladimir.uram@globallogic.com>
2017-08-11 10:33:00 +0200

    added entity admin

---
+++ src/main/java/com/project/webtemplate/entities/Admin.java
@@ -0,0 +1,24 @@
+package com.project.webtemplate.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Admin")
+public class Admin {
+    @Id
+    private int Id;
+
+    @Column(name = "User_Name")
+    private String userName;
+
+    @Column(name = "User_Password")
+    private String userPass;
+
+    public Admin(String userName, String userPass){
+        this.userName = userName;
+        this.userPass = userPass;
+    }
+}
