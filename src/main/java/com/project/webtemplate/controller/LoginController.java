2bc3021c1d9be6877737f691d36a3586c17120be
RRuscak <rastislav.ruscak@globallogic.com>
2017-08-11 12:28:06 +0200

    added postgres database

--- src/main/java/com/project/webtemplate/controller/LoginController.java
+++ src/main/java/com/project/webtemplate/controller/LoginController.java
@@ -3,12 +3,12 @@ package com.project.webtemplate.controller;
 import com.project.webtemplate.entities.Admin;
 import com.project.webtemplate.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class LoginController {
 
--- src/main/java/com/project/webtemplate/entities/Admin.java
+++ src/main/java/com/project/webtemplate/entities/Admin.java
@@ -23,5 +23,27 @@ public class Admin {
         this.userPass = userPass;
     }
 
+    public Integer getId() {
+        return Id;
+    }
 
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
 }
