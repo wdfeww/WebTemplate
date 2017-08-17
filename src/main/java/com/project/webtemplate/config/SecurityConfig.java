7c6a6ec531fcf3856a81cbd9cd6a48c6a668558c
wdfeww <wdfeww@gmail.com>
2017-08-17 13:33:46 +0200

    pac

--- src/main/java/com/project/webtemplate/config/SecurityConfig.java
+++ src/main/java/com/project/webtemplate/config/SecurityConfig.java
@@ -5,17 +5,15 @@ import com.project.webtemplate.config.jwt.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.AuthorizedUrl;
 
 @SuppressWarnings("SpringJavaAutowiringInspection")
 @Configuration
@@ -53,43 +51,67 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         return new JwtAuthenticationTokenFilter();
     }
 
-
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                // we don't need CSRF because our token is invulnerable
-//                .csrf().disable()
+    protected void configure(HttpSecurity http) throws Exception{
+       ((HttpSecurity)((AuthorizedUrl)
+                http.authorizeRequests()
+                        .antMatchers("/admin/**"))
+                .authenticated().and()).csrf().and()
+                    .formLogin()
+                        .loginPage("/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .successForwardUrl("/admin")
+                        .failureForwardUrl("/login")
+                        .permitAll().and()
+                .httpBasic();
 
-//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-
-                // don't create session
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-
+//
+//        ((HttpSecurity)((HttpSecurity)((AuthorizedUrl)http
 //                .authorizeRequests()
-                //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-
-                // allow anonymous resource requests
-//                .antMatchers(
-//                        HttpMethod.GET,
-//                        "/",
-//                        "/*.html",
-//                        "/favicon.ico",
-//                        "/**/*.html",
-//                        "/**/*.css",
-//                        "/**/*.js"
-//                ).permitAll()
-                .authorizeRequests()
-                    .antMatchers("**/admin").authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/login").permitAll();
-
-        // Custom JWT based security filter
-        http
-                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
-
-        // disable page caching
-        http.headers().cacheControl();
-
+//                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+//                .antMatchers("/root/**").access("hasRole('ROOT')")
+//        ).authenticated().and()).formLogin().loginPage("/login").permitAll()).httpBasic();
+//
     }
+
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                // we don't need CSRF because our token is invulnerable
+////                .csrf().disable()
+//
+////                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//
+//                // don't create session
+////                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//
+////                .authorizeRequests()
+//                //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//
+//                // allow anonymous resource requests
+////                .antMatchers(
+////                        HttpMethod.GET,
+////                        "/",
+////                        "/*.html",
+////                        "/favicon.ico",
+////                        "/**/*.html",
+////                        "/**/*.css",
+////                        "/**/*.js"
+////                ).permitAll()
+//                .authorizeRequests()
+//                    .antMatchers("**/admin").authenticated()
+//                    .and()
+//                .formLogin()
+//                    .loginPage("/login").permitAll();
+//
+//        // Custom JWT based security filter
+//        http
+//                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+//
+//        // disable page caching
+//        http.headers().cacheControl();
+//
+//    }
 }
--- src/main/java/com/project/webtemplate/controller/AuthenticationRestController.java
+++ src/main/java/com/project/webtemplate/controller/AuthenticationRestController.java
@@ -1,6 +1,5 @@
 package com.project.webtemplate.controller;
 
-import com.project.webtemplate.config.jwt.JwtAuthenticationRequest;
 import com.project.webtemplate.config.jwt.service.JwtAuthenticationResponse;
 import com.project.webtemplate.config.jwt.JwtTokenUtil;
 import com.project.webtemplate.config.jwt.JwtUser;
@@ -37,41 +36,33 @@ public class AuthenticationRestController {
 
     @RequestMapping(value = "${jwt.route.authentication.path}", method = RequestMethod.POST)
     public String createAuthenticationToken(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, Device device) throws AuthenticationException {
-
-        JwtAuthenticationRequest authenticationRequest = new JwtAuthenticationRequest();
-        authenticationRequest.setUsername(username);
-        authenticationRequest.setPassword(password);
-        System.out.println(username);
-        System.out.println(password);
         final Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        authenticationRequest.getUsername(),
-                        authenticationRequest.getPassword()
-                )
+                new UsernamePasswordAuthenticationToken(username, password)
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         // Reload password post-security so we can generate token
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-        final String token = jwtTokenUtil.generateToken(userDetails, device);
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+//        final String token =
 
+        jwtTokenUtil.generateToken(userDetails, device);
         // Return the token
 //        return ResponseEntity.ok(new JwtAuthenticationResponse(token));
-        return "admin";
+        return "redirect:admin";
     }
 
-    @RequestMapping(value = "${jwt.route.authentication.refresh}", method = RequestMethod.GET)
-    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
-        String token = request.getHeader(tokenHeader);
-        String username = jwtTokenUtil.getUsernameFromToken(token);
-        JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
-
-        if (jwtTokenUtil.canTokenBeRefreshed(token, null)) {
-            String refreshedToken = jwtTokenUtil.refreshToken(token);
-            return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
-        } else {
-            return ResponseEntity.badRequest().body(null);
-        }
-    }
+//    @RequestMapping(value = "${jwt.route.authentication.refresh}", method = RequestMethod.GET)
+//    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
+//        String token = request.getHeader(tokenHeader);
+//        String username = jwtTokenUtil.getUsernameFromToken(token);
+//        JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
+//
+//        if (jwtTokenUtil.canTokenBeRefreshed(token, null)) {
+//            String refreshedToken = jwtTokenUtil.refreshToken(token);
+//            return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
+//        } else {
+//            return ResponseEntity.badRequest().body(null);
+//        }
+//    }
 
 }
